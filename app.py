fa58722ec37c690a8dbf0150b47e9cedfda636bf
Bohyun Jung <orange@Bohyunui-MacBookAir.local>
2019-09-08 02:13:54 +0900

    Prevent duplicated matches

--- app.py
+++ app.py
@@ -1,35 +1,54 @@
 import random
-from flask import Flask, jsonify
+import json
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
 names = []
+old_matches = {}
+
 
 def loadFiles():
-    names = []
+    global names, old_matches
     with open("names.txt", "r") as f:
         names = [n.strip() for n in f.readlines()]
-    return names
+    with open("old_matches.txt", "r") as f:
+        old_matches_data = json.load(f)
+        old_matches = {(match[0], match[1]): True for match in old_matches_data if len(match) == 2}
+    print("names loaded:", names)
+    print("old_matches loaded:", old_matches)
+
 
 def generate_matches():
-    random.shuffle(names)
-    matches = [names[i:i+2] for i in range(0, len(names), 2)]
+    while True:
+        random.shuffle(names)
+        matches = [names[i:i+2] for i in range(0, len(names), 2)]
+        for match in matches:
+            if tuple(match) in old_matches or tuple(match[::-1]) in old_matches:
+                break
+        else:
+            return matches
 
-    return matches
+
+@app.route('/save', methods = ['POST'])
+def save():
+    with open("old_matches.txt", "r") as f:
+        old_matches_data = json.load(f)
+    old_matches_data += request.get_json(force=True)
+    with open("old_matches.txt", "w") as f:
+        json.dump(old_matches_data, f)
+    loadFiles()
+    return "OK"
+
 
 @app.route('/')
 def generate():
     return jsonify(generate_matches())
 
-if __name__ == '__main__':
 
-    names = loadFiles()
-    print("names loaded")
-    print(names)
+if __name__ == '__main__':
+    loadFiles()
 
     app.run(host='0.0.0.0')
-
-
-# save in (name1, name2): true dict
